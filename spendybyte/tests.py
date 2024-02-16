fcd46d3a890696f4837bd02f41c6ca5c2dcb95ce
ajinzrathod <ARathod@republicfinance.com>
2024-02-16 22:06:38 +0530

    Added tests

--- spendybyte/tests.py
+++ spendybyte/tests.py
@@ -2,19 +2,15 @@ import environ
 from django.contrib.auth.password_validation import validate_password
 import pytest
 
-from django.db import connection
-
 
 @pytest.mark.django_db
 def test_secret_key_strength():
-    print(">>>>>>>>>>>>>>>>", connection.settings_dict["NAME"])
-    assert 1 == 1
-    # env = environ.Env()
-    # environ.Env.read_env()
-    # SECRET_KEY = env("SECRET_KEY")
+    env = environ.Env()
+    environ.Env.read_env()
+    SECRET_KEY = env("SECRET_KEY")
 
-    # try:
-    #     validate_password(SECRET_KEY)
-    # except Exception as e:
-    #     exception = f"Bad Secret Key {e.messages}"
-    #     pytest.fail(exception)
+    try:
+        validate_password(SECRET_KEY)
+    except Exception as e:
+        exception = f"Bad Secret Key {e.messages}"
+        pytest.fail(exception)
