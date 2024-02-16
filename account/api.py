98d61f888b1bb6044ef652dbdf426d74330ad7d0
ajinzrathod <ARathod@republicfinance.com>
2024-02-16 22:18:03 +0530

    Add test using pytest and create custom user model, configured workflow file

--- account/api.py
+++ account/api.py
@@ -1,41 +1,41 @@
-# from rest_framework import generics, viewsets
-# from rest_framework.response import Response
+from rest_framework import generics, viewsets
+from rest_framework.response import Response
 
-# # from account.models import Account
-# from account.permissions import IsStaff
-# from .serializer import (
-#     RegisterSerializer,
-#     UserSerializer,
-#     UserSerializerRegisterResponse,
-# )
-# from rest_framework import status
-# from rest_framework_simplejwt.authentication import JWTAuthentication
-
-
-# class RegisterUserApi(generics.GenericAPIView):
-#     serializer_class = RegisterSerializer
-
-#     def post(self, request, *args, **kwargs):
-#         serializer = self.get_serializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         user = serializer.save()
-
-#         return Response(
-#             {
-#                 "success": True,
-#                 "data": UserSerializerRegisterResponse(
-#                     user, context=self.get_serializer_context()
-#                 ).data,
-#             }
-#         )
+from account.models import Account
+from account.permissions import IsStaff
+from .serializer import (
+    RegisterSerializer,
+    UserSerializer,
+    UserSerializerRegisterResponse,
+)
+from rest_framework import status
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
-# # class UserApi(viewsets.ModelViewSet):
-# #     authentication_classes = (JWTAuthentication,)
-# #     permission_classes = (IsStaff,)
+class RegisterUserApi(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
 
-# #     queryset = Account.objects.all()
-# #     serializer_class = UserSerializer
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
 
-# #     def create(self, request, *args, **kwargs):
-# #         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return Response(
+            {
+                "success": True,
+                "data": UserSerializerRegisterResponse(
+                    user, context=self.get_serializer_context()
+                ).data,
+            }
+        )
+
+
+class UserApi(viewsets.ModelViewSet):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsStaff,)
+
+    queryset = Account.objects.all()
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
--- account/urls.py
+++ account/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 
-# from .api import RegisterUserApi, UserApi
+from .api import RegisterUserApi, UserApi
 from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
-# router.register(r'users', UserApi, basename='user')
+router.register(r"users", UserApi, basename="user")
 
 urlpatterns = [
-    # path("register/", RegisterUserApi.as_view()),
+    path("register/", RegisterUserApi.as_view()),
     path("", include(router.urls)),
 ]
